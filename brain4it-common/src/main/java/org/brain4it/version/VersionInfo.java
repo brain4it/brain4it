8b42c756082b3e2d878ec5cacace335b74cb49c1
realor <realor@santfeliu.cat>
2019-09-11 17:48:08 +0200

    Show more version information (commit count) in the about dialogs.

--- brain4it-common/src/main/java/org/brain4it/version/VersionInfo.java
+++ brain4it-common/src/main/java/org/brain4it/version/VersionInfo.java
@@ -1,31 +1,31 @@
 /*
  * Brain4it
- * 
+ *
  * Copyright (C) 2018, Ajuntament de Sant Feliu de Llobregat
- * 
- * This program is licensed and may be used, modified and redistributed under 
- * the terms of the European Public License (EUPL), either version 1.1 or (at 
- * your option) any later version as soon as they are approved by the European 
+ *
+ * This program is licensed and may be used, modified and redistributed under
+ * the terms of the European Public License (EUPL), either version 1.1 or (at
+ * your option) any later version as soon as they are approved by the European
  * Commission.
- * 
- * Alternatively, you may redistribute and/or modify this program under the 
- * terms of the GNU Lesser General Public License as published by the Free 
- * Software Foundation; either  version 3 of the License, or (at your option) 
- * any later version. 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *   
- * See the licenses for the specific language governing permissions, limitations 
+ *
+ * Alternatively, you may redistribute and/or modify this program under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either  version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the licenses for the specific language governing permissions, limitations
  * and more details.
- *   
- * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along 
- * with this program; if not, you may find them at: 
- *   
+ *
+ * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along
+ * with this program; if not, you may find them at:
+ *
  *   https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
- *   http://www.gnu.org/licenses/ 
- *   and 
+ *   http://www.gnu.org/licenses/
+ *   and
  *   https://www.gnu.org/licenses/lgpl.txt
  */
 package org.brain4it.version;
@@ -42,8 +42,9 @@ public class VersionInfo
   public static final String CREDITS =
     "Copyright (C) 2018, Ajuntament de Sant Feliu de Llobregat";
   public static final String VERSION_INFO_PATH = "/git.properties";
-    
-  public static String getLastCommitDate()
+  private static final Properties properties = new Properties();
+
+  static
   {
     try
     {
@@ -52,9 +53,7 @@ public class VersionInfo
       {
         try
         {
-          Properties properties = new Properties();
           properties.load(is);
-          return properties.getProperty("git.commit.time");
         }
         finally
         {
@@ -66,6 +65,38 @@ public class VersionInfo
     {
       // ignore: git.properties not present
     }
-    return null;
-  }  
+  }
+
+  public static String getVersionString()
+  {
+    String buildVersion = getBuildVersion();
+    String commitCount = getCommitCount();
+    String lastCommitDate = getLastCommitDate();
+
+    if (buildVersion == null || commitCount == null || lastCommitDate == null)
+      return null;
+
+    return "Version " + buildVersion + " commit " + commitCount + " (" +
+      lastCommitDate + ")";
+  }
+
+  public static String getBuildVersion()
+  {
+    return properties.getProperty("git.build.version");
+  }
+
+  public static String getCommitCount()
+  {
+    return properties.getProperty("git.total.commit.count");
+  }
+
+  public static String getLastCommitDate()
+  {
+    return properties.getProperty("git.commit.time");
+  }
+
+  public static String getProperty(String propertyName)
+  {
+    return properties.getProperty(propertyName);
+  }
 }
--- brain4it-manager/brain4it-manager-swing/src/main/java/org/brain4it/manager/swing/AboutDialog.java
+++ brain4it-manager/brain4it-manager-swing/src/main/java/org/brain4it/manager/swing/AboutDialog.java
@@ -1,31 +1,31 @@
 /*
  * Brain4it
- * 
+ *
  * Copyright (C) 2018, Ajuntament de Sant Feliu de Llobregat
- * 
- * This program is licensed and may be used, modified and redistributed under 
- * the terms of the European Public License (EUPL), either version 1.1 or (at 
- * your option) any later version as soon as they are approved by the European 
+ *
+ * This program is licensed and may be used, modified and redistributed under
+ * the terms of the European Public License (EUPL), either version 1.1 or (at
+ * your option) any later version as soon as they are approved by the European
  * Commission.
- * 
- * Alternatively, you may redistribute and/or modify this program under the 
- * terms of the GNU Lesser General Public License as published by the Free 
- * Software Foundation; either  version 3 of the License, or (at your option) 
- * any later version. 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *   
- * See the licenses for the specific language governing permissions, limitations 
+ *
+ * Alternatively, you may redistribute and/or modify this program under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either  version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the licenses for the specific language governing permissions, limitations
  * and more details.
- *   
- * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along 
- * with this program; if not, you may find them at: 
- *   
+ *
+ * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along
+ * with this program; if not, you may find them at:
+ *
  *   https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
- *   http://www.gnu.org/licenses/ 
- *   and 
+ *   http://www.gnu.org/licenses/
+ *   and
  *   https://www.gnu.org/licenses/lgpl.txt
  */
 
@@ -51,13 +51,13 @@ public class AboutDialog extends javax.swing.JDialog
     initDialog();
     loadProperties();
   }
-  
+
   private void initDialog()
   {
     creditsLabel.setText(VersionInfo.CREDITS);
     int scalingFactor = ManagerApp.getPreferences().getScalingFactor();
     propertiesTable.setRowHeight(20 * scalingFactor);
-    
+
     ResourceBundle bundle = ResourceBundle.getBundle(
       "org/brain4it/manager/swing/resources/Manager");
     DefaultTableModel model = new DefaultTableModel();
@@ -69,11 +69,11 @@ public class AboutDialog extends javax.swing.JDialog
       setHeaderValue(bundle.getString("About.property"));
     propertiesTable.getColumnModel().getColumn(1).
       setHeaderValue(bundle.getString("About.value"));
-    
-    String commitDate = VersionInfo.getLastCommitDate();
-    if (commitDate != null)
+
+    String versionString = VersionInfo.getVersionString();
+    if (versionString != null)
     {
-      versionLabel.setText("Build " + commitDate);
+      versionLabel.setText(versionString);
     }
   }
 
@@ -89,7 +89,7 @@ public class AboutDialog extends javax.swing.JDialog
       {name, String.valueOf(properties.get(name))});
     }
   }
-  
+
   /**
    * This method is called from within the constructor to initialize the form.
    * WARNING: Do NOT modify this code. The content of this method is always

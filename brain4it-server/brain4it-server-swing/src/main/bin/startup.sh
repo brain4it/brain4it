1171c2a755b348a7f2d79a2e2228f3e0b92f5c61
raneq <raneq@posteo.net>
2020-10-27 11:14:24 +0100

    Make bash wrappers executable from any directory

--- brain4it-server/brain4it-server-swing/src/main/bin/startup.sh
+++ brain4it-server/brain4it-server-swing/src/main/bin/startup.sh
@@ -1,3 +1,4 @@
 # Brain4it server startup
 
+cd $(dirname $0)
 java -cp "../lib/*" -Djava.util.logging.manager=org.brain4it.server.standalone.ServerLogManager -Djava.util.logging.config.file="../conf/logging.properties" org.brain4it.server.swing.SwingRunner ../conf/server.properties

218107442c0646d4614f0b420d0e1ca11610ed2b
quergf <quergf@portatil>
2021-02-11 20:21:11 +0100

    Skeleton for kafka lib

---
+++ brain4it-lib/brain4it-lib-kafka/src/main/java/org/brain4it/lib/kafka/KafkaSendFunction.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.brain4it.lib.kafka;
+
+/**
+ *
+ * @author quergf
+ */
+public class KafkaSendFunction {
+    
+}

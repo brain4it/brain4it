e158d31bc30a76a01c11133e6ddab63298cae47f
raneq <raneq@posteo.net>
2021-03-01 16:02:47 +0100

    Remove subscription from kafka-poll

--- brain4it-lib/brain4it-lib-kafka/src/main/java/org/brain4it/lib/kafka/KafkaPollFunction.java
+++ brain4it-lib/brain4it-lib-kafka/src/main/java/org/brain4it/lib/kafka/KafkaPollFunction.java
@@ -5,12 +5,10 @@
  */
 package org.brain4it.lib.kafka;
 
-import java.util.ArrayList;
-import static java.util.Arrays.asList;
+import java.util.Set;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.brain4it.lang.BList;
 import org.brain4it.lang.Context;
 import org.brain4it.lang.Function;
@@ -21,9 +19,8 @@ import org.brain4it.lib.KafkaLibrary;
  *
  * @author quergf
  */
-public class KafkaPollFunction implements Function{
-    
-    
+public class KafkaPollFunction implements Function {
+
     protected KafkaLibrary library;
 
     public KafkaPollFunction(KafkaLibrary library) {
@@ -32,33 +29,36 @@ public class KafkaPollFunction implements Function{
 
     @Override
     public BList invoke(Context context, BList args) throws Exception {
-        Utils.checkArguments(args, 3);
+        Utils.checkArguments(args, 2);
         String consumerId = (String) context.evaluate(args.get(1));
-        String topic = (String) context.evaluate(args.get(2));
-        Long timeout = new Long((Integer) context.evaluate(args.get(3)));
-        
+        Long timeout = new Long((Integer) context.evaluate(args.get(2)));
+
         // Check arguments
         KafkaConsumer cons = (KafkaConsumer) library.getApp(consumerId);
         if (cons == null) {
             throw new java.lang.Exception("Consumer id not found");
         }
-        
-        // Subscribe to topics
-        cons.subscribe(asList(topic));
-        
+
         // Build BList of records, where:
         // - BList name is Kafka records's id
         // - BList element is Kafka record's value
         BList result = new BList();
         ConsumerRecords records = cons.poll(timeout);
-        for (ConsumerRecord record: (Iterable<ConsumerRecord>) records.records(topic)) {
-            if (record.key() != null)
-                result.put(record.key(), record.value());
-            else
-                result.add(record.value());
+        Set<String> topics = cons.subscription();
+
+        for (String topic : topics) {
+            BList topicRecords = new BList();
+            for (ConsumerRecord record : (Iterable<ConsumerRecord>) records.records(topic)) {
+                if (record.key() != null) {
+                    topicRecords.put(record.key(), record.value());
+                } else {
+                    topicRecords.add(record.value());
+                }
+            }
+            result.put(topic, topicRecords);
         }
-        
+
         return result;
     }
-    
+
 }
